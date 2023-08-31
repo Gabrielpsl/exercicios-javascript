a86e4e82f8902bf89c7cf0bf498ef9f0e87d65ce
Gabrielpsl <pcsolucoes.tec@gmail.com>
2023-08-30 23:54:54 -0400

    first commit

---
+++ exercicio 1/index.js
@@ -0,0 +1,28 @@
+// Atividade 1  criar uma string para aparecer no console
+
+// console.log("Hello Word!")
+// console.log('Meu nome é Gabriel')
+// console.log(`Estou animado com o curso`)
+
+// Atividade 2 criar 3 valores em number e exibi no navegador
+
+// console.log(1);
+// console.log(20.5);
+// console.log((3 *(12+4)) / 8);
+
+// Atividade 3 criar 3 comprarações com boolean
+
+// console.log(3>5); /* mair que */
+// console.log(7<=4);/* menor ou igual a*/
+// console.log(4!=7); /* igual ou diferente */
+
+// Atividade 4 Escreva tres comparações cim operadores lógicos com and or e not;
+
+// console.log(3 == 3 && "juão" != "juão");
+// console.log(4<=7 ||  3<2);
+// console.log(!(4 === 3));
+
+// Atividade 5 Faça uma operação que emita NaN no console.
+
+console.log(NaN)
+console.log(4*"string")
